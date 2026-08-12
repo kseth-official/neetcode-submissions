66a56775ef1fc11b0c96b4c690d19f2d8e80cbd0
Kush <80090780+kseth-official@users.noreply.github.com>
2026-08-12 13:55:09 -0700

    Add: majority-element-ii - submission-2

---
+++ Data Structures & Algorithms/majority-element-ii/submission-2.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        unordered_map<int,int> count;
+
+        for (const auto& num: nums) {
+            count[num]++;
+
+            if (count.size() > 2) {
+                unordered_map<int,int> new_count;
+
+                for (const auto& kv: count) {
+                    if (kv.second > 1)
+                        new_count[kv.first] = kv.second - 1;
+                }
+
+                count = new_count;
+            }
+        }
+
+        for (auto& kv: count) {
+            kv.second = 0;    
+        }
+
+        unordered_set<int> result;
+        for (const auto& num: nums) {
+            if (count.count(num) > 0) {
+                count[num]++;
+            } else {
+                continue;
+            }
+
+            if (count[num] > nums.size() / 3)
+                result.insert(num);
+        }
+
+
+        return vector<int>(result.begin(), result.end());
+    }
+};
