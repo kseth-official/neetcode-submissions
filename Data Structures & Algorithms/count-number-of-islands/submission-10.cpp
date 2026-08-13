06921d39df083e75e1c8039a5a42e35a5ddb0dbf
Kush <80090780+kseth-official@users.noreply.github.com>
2026-08-13 09:28:32 -0700

    Add: count-number-of-islands - submission-10

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-10.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<pair<int,int>> directions = {{0,-1}, {0,1}, {1,0}, {-1,0}};
+    
+    int numIslands(vector<vector<char>>& grid) {
+        int count = 0;
+
+        for (int i=0;i<grid.size();i++) {
+            for (int j=0;j<grid[0].size();j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(i,j,grid);
+                }
+            }
+        }
+
+        return count;
+    }
+
+private:
+    void dfs(int i, int j, vector<vector<char>>& grid) {
+        grid[i][j] = '0';
+        for (const auto& d: directions) {
+            int nx = i + d.first;
+            int ny = j + d.second;
+            if (nx >= 0 && nx < grid.size() && ny >=0 && ny < grid[0].size() && grid[nx][ny] == '1') {
+                dfs(nx, ny, grid);
+            }
+        }
+    };
+};
