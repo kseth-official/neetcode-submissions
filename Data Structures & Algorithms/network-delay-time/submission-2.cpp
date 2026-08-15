b2e3a530bc62c0fe6da3454705f84dc5d17babdd
Kush <80090780+kseth-official@users.noreply.github.com>
2026-08-15 12:34:55 -0700

    Add: network-delay-time - submission-2

---
+++ Data Structures & Algorithms/network-delay-time/submission-2.cpp
@@ -0,0 +1,46 @@
+class Solution {
+   public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        unordered_map<int, vector<pair<int,int>>> adj_list;
+
+        for (int i = 0; i < times.size(); i++) {
+            adj_list[times[i][0]].push_back({times[i][1], times[i][2]});
+        }
+
+        int int_max = numeric_limits<int>::max();
+        vector<int> distances(n, int_max);
+        distances[k-1]=0;
+
+        // min-heap of shortest distances to every vertex
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int,int>>> q;
+        q.push({k, 0});
+
+        while (!q.empty()) {
+            auto [ui, d] = q.top();
+            q.pop();
+
+            // -1 adjustment since the nodes are 1 indexed while distances are 0 indexed
+            if (d > distances[ui - 1]) {
+                continue;
+            }
+
+            for (const auto& [vi, ti]: adj_list[ui]) {
+                int new_dist = distances[ui-1] + ti;
+                if (new_dist < distances[vi-1]) {
+                    distances[vi-1] = new_dist;
+                    q.push({vi, new_dist});
+                }
+            }
+        }
+
+        // If maximum minimum distance to a node is infinite, it means that node was unreachable so its distance was never updated
+        int result = -1;
+        for (const auto& dist: distances) {
+            if (dist == int_max)
+                return -1;
+            result = max(result, dist);
+        }
+        
+        return result; 
+    }
+};
