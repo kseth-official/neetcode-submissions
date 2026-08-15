e44878b19eaf28f4b87af02e517fc88f1a4a6b3a
Kush <80090780+kseth-official@users.noreply.github.com>
2026-08-15 13:24:00 -0700

    Add: two-integer-sum - submission-13

---
+++ Data Structures & Algorithms/two-integer-sum/submission-13.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> m;
+        for (int i=0;i<nums.size();i++) {
+            if (m.count(target-nums[i]) > 0) {
+                int j = m[target-nums[i]];
+                return {j, i};
+                break;
+            }
+            m[nums[i]]=i;       
+        }
+
+        return {};
+    }
+};
