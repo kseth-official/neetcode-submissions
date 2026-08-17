93482d1163c27558ff1762989570e15e8169f467
Kush <80090780+kseth-official@users.noreply.github.com>
2026-08-16 19:31:41 -0700

    Add: unique-email-addresses - submission-7

---
+++ Data Structures & Algorithms/unique-email-addresses/submission-7.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numUniqueEmails(vector<string>& emails) {
+        unordered_set<string> u;
+
+        for (const auto& email: emails) {
+            string local = email.substr(0, email.find('@'));
+            string dom = email.substr(email.find('@') + 1);
+            string uptoPlus = local.substr(0, local.find('+'));
+
+            string iDeleted = "";          
+            for (int j=0;j<uptoPlus.size();j++) {
+                if (uptoPlus[j] == '.') 
+                    continue;
+                iDeleted += uptoPlus[j];
+            }
+
+            u.insert(iDeleted + dom);
+        }
+
+        return u.size();
+    }
+};
