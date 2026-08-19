4464924964183df7848a63d3e4b49d574131c6a8
Kush <80090780+kseth-official@users.noreply.github.com>
2026-08-19 01:14:44 -0700

    Add: generate-parentheses - submission-8

---
+++ Data Structures & Algorithms/generate-parentheses/submission-8.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> sols;
+        string curr ="";
+        bt(0,0, n, curr, sols);
+        return sols;
+    }
+
+    void bt(int openCount, int closedCount, int n, string& curr, vector<string>& sols) {
+        if (openCount == n && closedCount ==n) {
+            sols.push_back(curr);
+            return;
+        }
+
+        if (openCount < n) {
+            curr.push_back('(');
+            bt(openCount+1, closedCount,n, curr, sols);
+            curr.pop_back();
+        }
+        if (closedCount < openCount) {
+            curr.push_back(')');
+            bt(openCount, closedCount+1,n, curr, sols);
+            curr.pop_back();
+        }
+    }
+};
