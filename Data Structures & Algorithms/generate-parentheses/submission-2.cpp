c42451f5f1f5f0b90719b0b3808ba7f5dc1963e7
Kush <80090780+kseth-official@users.noreply.github.com>
2026-08-19 00:59:32 -0700

    Add: generate-parentheses - submission-2

---
+++ Data Structures & Algorithms/generate-parentheses/submission-2.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> sols;
+        bt(0, n*2, "", sols);
+        return sols;
+    }
+
+    void bt(int i, int n, string curr, vector<string>& sols) {
+        if (i == n) {
+            if (isValid(curr)) {
+                sols.push_back(curr);
+            }
+            // base case for adding a solution
+            return;
+        }
+
+        curr.push_back('(');
+        bt(i+1,n,curr, sols);
+        curr.pop_back();
+        curr.push_back(')');
+        bt(i+1,n,curr, sols);
+    }
+
+
+    bool isValid(string par) {
+        stack<char> track;
+
+        for (const auto& p: par) {
+            if (p == '(') {
+                track.push(p);
+                continue;
+
+            }
+
+            if (p == ')') {
+                if (track.empty())
+                    return false;
+                auto t = track.top();
+                track.pop();
+            }
+        }
+
+        if (!track.empty())
+            return false;
+
+        return true;
+    }
+};
