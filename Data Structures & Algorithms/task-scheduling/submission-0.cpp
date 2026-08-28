dabf1a51799cb48152fc668341ce7bfd436f1e90
Kush <80090780+kseth-official@users.noreply.github.com>
2026-08-28 00:45:37 -0700

    Add: task-scheduling - submission-0

---
+++ Data Structures & Algorithms/task-scheduling/submission-0.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        auto cmp = [] (pair<int,int> a, pair<int,int> b) {
+            return a.second < b.second;
+        };
+
+        priority_queue<pair<int,int>, vector<pair<int,int>>, decltype(cmp)> pq;
+
+        unordered_map<char,int> fq;
+        for (int i=0;i<tasks.size();i++) {
+            fq[tasks[i]]++;
+        }
+
+        for (auto& p: fq) {
+            pq.push(p);
+        }
+
+        queue<pair<pair<int,int>,int>> q;
+
+        int t = 0;
+
+        while (!pq.empty() || !q.empty()) {
+            t++;
+            if (!q.empty()) {
+                auto top = q.front();
+
+                if (top.second == t) {
+                    q.pop();
+                    pq.push(top.first);
+                }
+            }
+            
+            if (!pq.empty()) {
+                auto top = pq.top();
+                pq.pop();
+                top.second--;
+
+                if (top.second != 0) {
+                    q.push({{top.first,top.second}, t+n+1});
+                }
+            }
+        }
+
+        return t;
+    }
+};
