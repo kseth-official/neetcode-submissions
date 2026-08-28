b881181cc8646450307d3b6be91b42dcdae0b066
Kush <80090780+kseth-official@users.noreply.github.com>
2026-08-27 23:29:38 -0700

    Add: unique-paths-ii - submission-5

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-5.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        if (obstacleGrid[0][0] == 1)
+            return 0;
+        
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+        dp[0][0]=1;
+
+        for (int i=0;i<n;i++) {
+            for (int j=0;j<m;j++) {
+                if (obstacleGrid[i][j] == 1) 
+                    continue;
+                if (i > 0) 
+                    dp[i][j] += dp[i-1][j];
+                if (j > 0) 
+                    dp[i][j] += dp[i][j-1];
+            }
+        }
+
+        return dp[n-1][m-1];
+    }
+};
