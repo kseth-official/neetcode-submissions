0e028737fd8c222a91f73e2b07571f15204117af
Kush <80090780+kseth-official@users.noreply.github.com>
2026-09-01 18:53:56 -0700

    Add: count-vowel-strings-in-ranges - submission-3

---
+++ Data Structures & Algorithms/count-vowel-strings-in-ranges/submission-3.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
+        // use prefix and postfix sums to allow O(n) time complexity
+        vector<int> pr;
+    
+        for (int i=0;i<words.size();i++) {
+            auto& word = words[i];
+            char& st = word[0];
+            char& end = word[word.size()-1];
+
+            int r = 0;
+            if (isVowel(st) && isVowel(end)) {
+                r = 1;
+            } 
+            
+            if (pr.empty())
+                pr.push_back(r);
+            else
+                pr.push_back(pr[i-1] + r);
+        }     
+
+        vector<int> result;
+        for (int i=0;i<queries.size();i++) {
+            int li = queries[i][0];
+            int ri = queries[i][1];
+
+            int k;
+            if (li == 0)
+                k = pr[ri];
+            else 
+                k = pr[ri]-pr[li-1];
+            result.push_back(k);
+        }
+
+        // 1 0 1 1 1
+
+        // 1 1 2 3 4
+
+        // 4 3 3 2 1
+
+        return result;
+    }
+
+    bool isVowel(char& l) {
+        return l == 'a' || l == 'e' || l == 'i' || l == 'o' || l == 'u';
+    }
+};
