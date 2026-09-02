6612711ae4d7e6ef039e7b6d2ba1df48ae86f8ff
Kush <80090780+kseth-official@users.noreply.github.com>
2026-09-01 19:22:12 -0700

    Add: permutation-string - submission-6

---
+++ Data Structures & Algorithms/permutation-string/submission-6.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s1.size() > s2.size())
+            return false;
+
+        int k = s1.size();
+
+        unordered_map<char,int> fq;
+        unordered_map<char,int> wfq;
+
+        for (int i=0;i<k;i++) {
+            fq[s1[i]]++;
+            wfq[s2[i]]++;
+        }
+
+        int l = 0;
+        int r = k-1;
+
+        while (r < s2.size()-1) {
+            if (equate(fq, wfq))
+                return true;
+            
+            wfq[s2[l]]--;
+            l++;
+            r++;
+            wfq[s2[r]]++;
+        } 
+
+        return equate(fq,wfq);
+    }
+
+    bool equate(unordered_map<char,int>& a, unordered_map<char,int>& b) {
+        for (const auto& p: a) {
+            if (b.count(p.first) == 0)
+                return false;
+            if (b[p.first] != p.second)
+                return false;
+        }
+
+        return true;
+    }
+};
