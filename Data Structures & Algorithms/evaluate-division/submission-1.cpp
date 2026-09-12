57cbd8481670766fa4adbbb566018c4ba6a07ea8
Kush <80090780+kseth-official@users.noreply.github.com>
2026-09-11 23:42:15 -0700

    Add: evaluate-division - submission-1

---
+++ Data Structures & Algorithms/evaluate-division/submission-1.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        // precompute a mapping of unique variable -> index
+        unordered_set<string> uniq;
+        unordered_map<string, int> m;
+        int p=0;
+        for (int i=0;i<equations.size();i++) {
+            string ai = equations[i][0];
+            string bi = equations[i][1];
+
+            uniq.insert(ai);
+            uniq.insert(bi);
+
+            if (m.count(ai) == 0) {
+                m[ai] = p;
+                p++;
+            }
+            if (m.count(bi) == 0) {
+                m[bi] = p;
+                p++;
+            }    
+        }
+        int n = uniq.size();
+
+        vector<vector<double>> reachable(n, vector<double>(n, 0));
+        // division is 0 not possible since values never 0. so if 0,
+        // not reachable
+
+        for (int i=0;i<equations.size();i++) {
+            auto& eq = equations[i];
+            auto& ai = eq[0];
+            auto& bi = eq[1];
+            reachable[m[ai]][m[bi]]=values[i];
+            reachable[m[bi]][m[ai]]=1/values[i];
+        }
+
+        // applying floyd warshall to calculate reachability and value when reached
+        for (int k=0;k<n;k++) {
+            for (int i=0;i<n;i++) {
+                for (int j=0;j<n;j++) {
+                    if (reachable[i][j] != 0)
+                        continue;
+                    // this works because we assume that there are no
+                    // contradictions in the equations
+                    reachable[i][j] = reachable[i][k]*reachable[k][j];
+                }
+            }
+        }
+
+        vector<double> soln;
+        for (auto& q: queries) {
+            string& cj = q[0];
+            string& dj = q[1];
+
+            if (uniq.count(cj) == 0 || uniq.count(dj) == 0 || reachable[m[cj]][m[dj]] == 0)
+                soln.push_back(-1.0);
+            else
+                soln.push_back(reachable[m[cj]][m[dj]]);
+        }
+
+        return soln;
+    }
+};
