06f6f248c97b09f0326f35d3d71da286830c449c
Kush <80090780+kseth-official@users.noreply.github.com>
2026-09-13 18:03:39 -0700

    Add: rotate-array - submission-1

---
+++ Data Structures & Algorithms/rotate-array/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        if (k % n == 0)
+            return;
+        k = k % n;
+        /*
+            12345678 k=0
+            81234567 k=1
+            78123456 k=2
+            67812345 k=3
+            56781234 k=4
+            45678123 k=5
+            34567812 k=6
+            23456781 k=7
+        */
+        vector<int> soln(n);
+        for (int i=0;i<n;i++) {
+            soln[(i+k)%n] = nums[i];
+        }
+
+        nums = soln;
+    }
+};
